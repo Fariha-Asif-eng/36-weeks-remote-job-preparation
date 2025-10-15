79dbef9b741941fcdd6115de775e0412559e17c9
Fariha Asif <syedf4852@gmail.com>
2025-10-15 18:34:55 +0000

    learning conversion operations

---
+++ week-01/JAVASCRIPT/01_basic/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+console.log(typeof (score)); // yeh method k zariye b maloom hota hy
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof valueInNumber);
+
+let score1 = "33abc"
+let conversion = Number(score1)
+console.log(conversion);
+console.log(typeof score1);
+console.log(typeof conversion);
+
+// javascript me yeh conversion hojati hy jb k yeh honi nahi chahiye isliye javascript type safety language nahi hy
+
+
+
+
