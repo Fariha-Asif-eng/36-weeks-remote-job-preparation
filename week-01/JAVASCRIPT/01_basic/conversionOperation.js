4032f7ed97fbf5aa76de8d12a8c24976ed1bd47f
Fariha Asif <syedf4852@gmail.com>
2025-11-19 18:12:45 +0000

    conversion of data types

--- week-01/JAVASCRIPT/01_basic/conversionOperation.js
+++ week-01/JAVASCRIPT/01_basic/conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33"
 
 console.log(typeof (score)); // yeh method k zariye b maloom hota hy
-console.log(typeof score);
+console.log(typeof score);  
 
 let valueInNumber = Number(score)
 console.log(typeof score);
@@ -13,7 +13,31 @@ console.log(conversion);
 console.log(typeof score1);
 console.log(typeof conversion);
 
-// javascript me yeh conversion hojati hy jb k yeh honi nahi chahiye isliye javascript type safety language nahi hy
+// javascript me yeh conversion hojati hy jb k yeh honi nahi chahiye isliye javascript type safety language nahi hy 
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "Fariha";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Fariha" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
 
 
 
